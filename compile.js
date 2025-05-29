23655e9ffdfb8133ebec3a9a35e0c34ed3ed12a7
Éric NICOLAS <ccjmne@gmail.com>
2025-05-29 21:25:51 +0200

    Tidy up compile.js

--- compile.js
+++ compile.js
@@ -1,13 +1,3 @@
 import { readFileSync } from 'fs';
-
 const i = String(readFileSync('main.js')).replace(/\n|^\s+/gm, '')
-process.stdout.write(
-  i.replace(
-    `s='^'`,
-    `s='${i
-      .replace(/\\/g, '\\\\')
-      .replace(/\n/g, '\\n')
-      .replace(/'/g, `\\\'`)
-    }'`,
-  ),
-)
+process.stdout.write(i.replace('^', i.replace(/\\/g, '\\\\').replace(/'/g, `\\\'`)))
